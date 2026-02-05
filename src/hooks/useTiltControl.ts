4fb72c3382e3d3ebf0b8600c4aa6135f85cea213
Vadim <vadimivanov.0320@gmail.com>
2026-02-05 23:35:57 +0200

    Lowered maximum speed even more

--- src/hooks/useTiltControl.ts
+++ src/hooks/useTiltControl.ts
@@ -2,7 +2,7 @@ import { MoveVector } from '@/types/control'
 import { useEffect, useRef, useState } from 'react'
 
 const LERP_SPEED = 0.009
-const MAX_Y = 5
+const MAX_Y = 50
 
 export function useTiltControl() {
 	const [movement, setMovement] = useState<MoveVector>({
